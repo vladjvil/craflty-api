7f641d34039bd2d63c2f53bb90a3b56d0a5bdf33
vcrmm <vladviluha@SashaLetko.local>
2024-12-31 00:56:00 +0300

    add poms

---
+++ craftly-user/src/main/java/com/craftly/craftlyuser/CraftlyUserApplication.java
@@ -0,0 +1,14 @@
+package com.craftly.craftlyuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CraftlyUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftlyUserApplication.class, args);
+	}
+
+}
